953020901b592a4bed58c5a909484fb512019786
mohamedreda-2 <mooreda.2030@gmail.com>
2024-09-20 11:29:35 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,1 @@
+let links ["Home", "About", "Blog", "Contact"];
---
+++ index.js
@@ -0,0 +1,6 @@
+
+let nav = document.querySelector("nav");
+links.forEach((links) => {
+    nav.innerHTML += `<a hrerf="">${links}</a>`;
+    
+});
